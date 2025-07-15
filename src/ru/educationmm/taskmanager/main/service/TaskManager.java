3f6cac2ff08e228b42b4a7d5a0106922a65b8a13
MaximM <mml999@ya.ru>
2025-07-16 01:39:31 +0300

    Проект 6 спринта, исправления после ревью + codestyle

--- src/ru/educationmm/taskmanager/main/service/TaskManager.java
+++ src/ru/educationmm/taskmanager/main/service/TaskManager.java
@@ -8,23 +8,42 @@ import java.util.List;
 
 public interface TaskManager {
     List<Task> getTasks();
+
     List<Epic> getEpics();
+
     List<Subtask> getSubtasks();
+
     boolean addTask(Task task);
+
     boolean addEpic(Epic epic);
+
     boolean addSubtask(Subtask subtask);
+
     boolean deleteAllTasks();
+
     boolean deleteAllEpics();
+
     boolean deleteAllSubtasks();
+
     Task getTaskById(int taskId);
+
     Epic getEpicById(int epicId);
+
     Subtask getSubtaskById(int subtaskId);
+
     boolean updateTask(Task task);
+
     boolean updateEpic(Epic epic);
+
     boolean updateSubtask(Subtask subtask);
+
     boolean deleteTaskById(int taskId);
+
     boolean deleteEpicById(int epicId);
+
     boolean deleteSubtaskById(int subtaskId);
+
     List<Subtask> getEpicSubtasks(int epicId);
+
     List<Task> getHistory();
 }
